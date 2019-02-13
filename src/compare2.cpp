62ac3c215f0f26cdc3c70a3f708ce318c07ca9aa
ZbyLGsc <405540572@qq.com>
2019-02-13 16:34:52 +0800

    add rrt front end

--- src/compare2.cpp
+++ src/compare2.cpp
@@ -3,7 +3,9 @@
 #include "std_msgs/Empty.h"
 
 #include <stdlib.h>
-#include "grad_traj_optimization/a_star.h"
+// #include "grad_traj_optimization/a_star.h"
+// #include <grad_traj_optimization/rrgPathFinder.h>
+#include <grad_traj_optimization/path_finder.h>
 #include "grad_traj_optimization/display.h"
 #include "grad_traj_optimization/grad_traj_optimizer.h"
 
@@ -21,50 +23,6 @@ double start_vx, start_vy, start_vz;
 double goal_x, goal_y, goal_z;
 
 bool have_goal, have_map;
-
-void simplifyPath(vector<Eigen::Vector3d> &path, SDFMap &sdf_map) {
-  vector<Eigen::Vector3d> path_sp;
-  path_sp.push_back(path.front());
-
-  // int cur_id = 1;
-  // while (cur_id < path.size()) {
-  //   Eigen::Vector3d pt1 = path_sp.back();
-  //   Eigen::Vector3d pt2 = path[cur_id];
-
-  //   /* detect any collision between pt1 and pt2 */
-  //   bool collision = false;
-  //   Eigen::Vector3d dir = pt2 - pt1;
-  //   double lambda = 0.1;
-  //   while (lambda < 1.0) {
-  //     Eigen::Vector3d ptm = pt1 + lambda * dir;
-  //     // int occ = sdf_map.getOccupancy(ptm);
-  //     double dist = sdf_map.getDistance(ptm);
-  //     if (dist < 0.4) {
-  //       collision = true;
-  //       break;
-  //     }
-  //     lambda += 0.1;
-  //   }
-
-  //   if (!collision) {
-  //     ++cur_id;
-  //   } else {
-  //     path_sp.push_back(path[cur_id - 1]);
-  //   }
-  // }
-  // path_sp.push_back(path.back());
-
-  const int pt_num = path.size();
-  int num = 10;
-  while (num < pt_num) {
-    path_sp.push_back(path[num]);
-    num += 10;
-  }
-
-  path_sp.push_back(path.back());
-
-  path = path_sp;
-}
 
 void mapCallback(const sensor_msgs::PointCloud2 &msg) {
   pcl::fromROSMsg(msg, latest_cloud);
@@ -99,12 +57,10 @@ int main(int argc, char **argv) {
 
   // -------------------ros initialization---------------------
   setpoint_pub =
-      node.advertise<visualization_msgs::Marker>("trajopt/setpoint", 10);
+      node.advertise<visualization_msgs::Marker>("grad_opt/setpoint", 10);
   traj_point_pub =
-      node.advertise<visualization_msgs::Marker>("trajopt/traj_point", 10);
-  traj_pub = node.advertise<nav_msgs::Path>("trajopt/init_traj", 5);
-  ros::Publisher visualization_pub = node.advertise<visualization_msgs::Marker>(
-      "sdf_tools_tutorial_visualization", 1, true);
+      node.advertise<visualization_msgs::Marker>("grad_opt/traj_point", 10);
+  traj_pub = node.advertise<nav_msgs::Path>("grad_opt/init_traj", 5);
 
   ros::Subscriber map_sub =
       node.subscribe("/laser_cloud_surround", 1, mapCallback);
@@ -125,14 +81,8 @@ int main(int argc, char **argv) {
                            Eigen::Vector3d(-40 / 2, -40 / 2, 0.0), 0.2);
 
   /* init path finder */
-  AStarPathFinder *path_finder = new AStarPathFinder();
-  Eigen::Vector3i gn;
-  gn(0) = ceil(40 / 0.2), gn(1) = ceil(40 / 0.2), gn(2) = ceil(5 / 0.2);
-  path_finder->initGridNodeMap(gn, 0.2, Eigen::Vector3d(-40 / 2, -40 / 2, 0.0));
-
-  /* init sdf */
-  SDFMap sdf_map(Eigen::Vector3d(-40 / 2, -40 / 2, 0.0), 0.2,
-                 Eigen::Vector3d(40, 40, 5));
+  rrtPathFinder path_finder;
+  path_finder.setParam(0.5, 0.2, 5.0, 10);
 
   /* main loop */
   while (ros::ok()) {
@@ -142,7 +92,7 @@ int main(int argc, char **argv) {
       ros::Duration(0.1).sleep();
       ros::spinOnce();
     }
-    cout << "[1]: Map and SG ok!" << endl;
+    cout << "[2]: Map and SG ok!" << endl;
 
     /* manage map */
     if (exp_num % use_map_num == 0) {
@@ -152,70 +102,74 @@ int main(int argc, char **argv) {
         obss.push_back(Eigen::Vector3d(pt.x, pt.y, pt.z));
       }
       grad_traj_opt.updateSDFMap(obss);
-
-      sdf_map.resetBuffer();
-      for (int i = 0; i < int(obss.size()); ++i) {
-        sdf_map.setOccupancy(obss[i]);
-      }
-      sdf_map.updateESDF3d();
-
-      path_finder->linkLocalMap(sdf_map);
+      path_finder.setInput(latest_cloud);
     }
 
-    // path finding
+    /* path finding */
     Eigen::Vector3d start, end;
     start(0) = start_x, start(1) = start_y, start(2) = start_z;
     end(0) = goal_x, end(1) = goal_y, end(2) = goal_z;
 
-    path_finder->resetNode();
-    path_finder->resetPath();
-    path_finder->searchPath(start, end);
+    Point point_s, point_g;
+    point_s.x = start_x, point_s.y = start_y, point_s.z = start_z;
+    point_g.x = goal_x, point_g.y = goal_y, point_g.z = goal_z;
 
-    vector<Eigen::Vector3d> path = path_finder->getPath();
+    ros::Time t1 = ros::Time::now();
 
-    /* simplify path */
-    cout << "path size bf sp: " << path.size() << endl;
-    simplifyPath(path, sdf_map);
-    if (path.size() == 2) {
-      path.clear();
-      const double lambda = 0.2;
-      for (int i = 0; i <= 5; ++i) {
-        Eigen::Vector3d mid_pt = (1 - lambda * i) * start + lambda * i * end;
-        path.push_back(mid_pt);
+    path_finder.reset();
+    path_finder.setPt(point_s, point_g, -20, 20, -20, 20, 0, 5, 0, 1, 10, 50000,
+                      0.15, 0.05);
+    path_finder.RRTpathFind(0.05);
+
+    // vector<Eigen::Vector3d> path = path_finder.getPath();
+    if (!path_finder.path_find_state) {
+      ROS_WARN("[2]: can't find a path");
+    } else {
+      ros::Time t2 = ros::Time::now();
+      double time_search = (t2 - t1).toSec();
+      cout << "time in search: " << time_search << endl;
+
+      Eigen::MatrixXd path_mat = path_finder.getPath().first;
+      cout << "path:\n" << path_mat << endl;
+      vector<Eigen::Vector3d> path;
+      for (int i = 0; i < path_mat.rows(); ++i) {
+        if (i == 1) continue;
+        if (i == path_mat.rows() - 2) continue;
+        Eigen::Vector3d wp = path_mat.row(i);
+        path.push_back(wp);
       }
+
+      if (path.size() == 2) {
+        Eigen::Vector3d mid = 0.5 * (path[0] + path[1]);
+        path.insert(path.begin() + 1, mid);
+      }
+      point_num = path.size();
+      cout << "p num: " << point_num << endl;
+
+      visualizeSetPoints(path);
+
+      /* generate traj */
+      t1 = ros::Time::now();
+      grad_traj_opt.setPath(path);
+
+      Eigen::MatrixXd coeff;
+      grad_traj_opt.getSegmentTime(my_time);
+      cout << "time: " << my_time.transpose() << endl;
+      grad_traj_opt.getCoefficient(coeff);
+      displayTrajectory(coeff, false);
+
+      // second step optimization
+      grad_traj_opt.optimizeTrajectory(OPT_SECOND_STEP);
+      grad_traj_opt.getCoefficient(coeff);
+      displayTrajectory(coeff, false);
+
+      t2 = ros::Time::now();
+      double time_opt = (t2 - t1).toSec();
+      cout << "time in opt: " << time_opt << endl;
+
+      cout << "total_time: " << time_search + time_opt << endl;
+      cout << "[2]: finish test." << endl;
     }
-    cout << "path size af sp: " << path.size() << endl;
-
-    point_num = path.size();
-
-    /* straight line initialization */
-    // const double lambda = 0.2;
-    // for (int i = 0; i <= 5; ++i) {
-    //   Eigen::Vector3d mid_pt = (1 - lambda * i) * start + lambda * i * end;
-    //   path.push_back(mid_pt);
-    // }
-
-    //  display  waypoints in rviz
-    visualizeSetPoints(path);
-    cout << "Way points created" << endl;
-
-    /* generate traj */
-    grad_traj_opt.setPath(path);
-
-    Eigen::MatrixXd coeff;
-    grad_traj_opt.getSegmentTime(my_time);
-    grad_traj_opt.getCoefficient(coeff);
-    displayTrajectory(coeff, false);
-
-    // // first step optimization
-    // grad_traj_opt.optimizeTrajectory(OPT_FIRST_STEP);
-    // grad_traj_opt.getCoefficient(coeff);
-    // displayTrajectory(coeff, false);
-
-    // second step optimization
-    grad_traj_opt.optimizeTrajectory(OPT_SECOND_STEP);
-    grad_traj_opt.getCoefficient(coeff);
-    displayTrajectory(coeff, false);
 
     /* finish test flag */
     ++exp_num;
@@ -225,7 +179,7 @@ int main(int argc, char **argv) {
     std_msgs::Empty finish_msg;
     finish_pub.publish(finish_msg);
 
-    ros::Duration(0.5).sleep();
+    // ros::Duration(0.5).sleep();
   }
 
   return 0;
