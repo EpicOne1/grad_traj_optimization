52c701a956c1d7ada7551d40d37dec2c3d3755d3
ZbyLGsc <405540572@qq.com>
2019-02-14 00:59:30 +0800

    add polytraj

--- src/compare2.cpp
+++ src/compare2.cpp
@@ -24,6 +24,91 @@ double goal_x, goal_y, goal_z;
 
 bool have_goal, have_map;
 
+ros::Publisher poly_traj_pub;
+
+void displayPathWithColor(vector<Eigen::Vector3d> path, double resolution,
+                          Eigen::Vector4d color, int id) {
+  visualization_msgs::Marker mk;
+  mk.header.frame_id = "world";
+  mk.header.stamp = ros::Time::now();
+  mk.type = visualization_msgs::Marker::SPHERE_LIST;
+  mk.action = visualization_msgs::Marker::DELETE;
+  mk.id = id;
+  poly_traj_pub.publish(mk);
+
+  mk.action = visualization_msgs::Marker::ADD;
+  mk.pose.orientation.x = 0.0, mk.pose.orientation.y = 0.0,
+  mk.pose.orientation.z = 0.0, mk.pose.orientation.w = 1.0;
+  mk.scale.x = resolution, mk.scale.y = resolution, mk.scale.z = resolution;
+
+  mk.color.r = color(0), mk.color.g = color(1), mk.color.b = color(2),
+  mk.color.a = color(3);
+
+  geometry_msgs::Point pt;
+  for (int i = 0; i < int(path.size()); i++) {
+    pt.x = path[i](0), pt.y = path[i](1), pt.z = path[i](2);
+    mk.points.push_back(pt);
+  }
+  poly_traj_pub.publish(mk);
+  ros::Duration(0.01).sleep();
+}
+
+class PolynomialTraj {
+ private:
+  vector<double> times;        // time of each segment
+  vector<vector<double>> cxs;  // coefficient of x of each segment
+  vector<vector<double>> cys;  // coefficient of y of each segment
+  vector<vector<double>> czs;  // coefficient of z of each segment
+
+  double time_sum;
+  int num_seg;
+
+ public:
+  PolynomialTraj(/* args */) {}
+  ~PolynomialTraj() {}
+
+  void reset() {
+    times.clear(), cxs.clear(), cys.clear(), czs.clear();
+    time_sum = 0.0, num_seg = 0;
+  }
+
+  void addSegment(vector<double> cx, vector<double> cy, vector<double> cz,
+                  double t) {
+    cxs.push_back(cx), cys.push_back(cy), czs.push_back(cz), times.push_back(t);
+  }
+
+  void init() {
+    num_seg = times.size();
+    time_sum = 0.0;
+    for (int i = 0; i < times.size(); ++i) {
+      time_sum += times[i];
+    }
+  }
+
+  double getTimeSum() { return this->time_sum; }
+
+  Eigen::Vector3d evaluate(double t) {
+    /* detetrmine segment num */
+    int idx = 0;
+    while (times[idx] < t) {
+      t -= times[idx];
+      ++idx;
+    }
+
+    /* evaluation */
+    int order = cxs[idx].size();
+    Eigen::VectorXd cx(order), cy(order), cz(order), tv(order);
+    for (int i = 0; i < order; ++i) {
+      cx(i) = cxs[idx][i], cy(i) = cys[idx][i], cz(i) = czs[idx][i];
+      tv(order - 1 - i) = std::pow(t, double(i));
+    }
+
+    Eigen::Vector3d pt;
+    pt(0) = tv.dot(cx), pt(1) = tv.dot(cy), pt(2) = tv.dot(cz);
+    return pt;
+  }
+};
+
 void mapCallback(const sensor_msgs::PointCloud2 &msg) {
   pcl::fromROSMsg(msg, latest_cloud);
 
@@ -60,7 +145,9 @@ int main(int argc, char **argv) {
       node.advertise<visualization_msgs::Marker>("grad_opt/setpoint", 10);
   traj_point_pub =
       node.advertise<visualization_msgs::Marker>("grad_opt/traj_point", 10);
-  traj_pub = node.advertise<nav_msgs::Path>("grad_opt/init_traj", 5);
+  // traj_pub = node.advertise<nav_msgs::Path>("grad_opt/init_traj", 5);
+  poly_traj_pub = node.advertise<visualization_msgs::Marker>(
+      "/gradient_based/traj", 10, true);
 
   ros::Subscriber map_sub =
       node.subscribe("/laser_cloud_surround", 1, mapCallback);
@@ -150,24 +237,52 @@ int main(int argc, char **argv) {
 
       /* generate traj */
       t1 = ros::Time::now();
-      grad_traj_opt.setPath(path);
 
+      grad_traj_opt.setPath(path);
       Eigen::MatrixXd coeff;
-      grad_traj_opt.getSegmentTime(my_time);
-      cout << "time: " << my_time.transpose() << endl;
-      grad_traj_opt.getCoefficient(coeff);
-      displayTrajectory(coeff, false);
+      Eigen::VectorXd time_sgm;
+      grad_traj_opt.getSegmentTime(time_sgm);
+      my_time = time_sgm;
+      // grad_traj_opt.getCoefficient(coeff);
+      // displayTrajectory(coeff, false);
 
       // second step optimization
       grad_traj_opt.optimizeTrajectory(OPT_SECOND_STEP);
       grad_traj_opt.getCoefficient(coeff);
-      displayTrajectory(coeff, false);
 
       t2 = ros::Time::now();
       double time_opt = (t2 - t1).toSec();
       cout << "time in opt: " << time_opt << endl;
-
       cout << "total_time: " << time_search + time_opt << endl;
+
+      /* convert coefficient to poly_traj */
+      PolynomialTraj poly_traj;
+      for (int i = 0; i < coeff.rows(); ++i) {
+        vector<double> cx(6), cy(6), cz(6);
+        for (int j = 0; j < 6; ++j) {
+          cx[j] = coeff(i, j), cy[j] = coeff(i, j + 6),
+          cz[j] = coeff(i, j + 12);
+        }
+        reverse(cx.begin(), cx.end());
+        reverse(cy.begin(), cy.end());
+        reverse(cz.begin(), cz.end());
+        double ts = time_sgm(i);
+        poly_traj.addSegment(cx, cy, cz, ts);
+      }
+      poly_traj.init();
+
+      double time_sum = poly_traj.getTimeSum();
+      double eval_t = 0.0;
+      vector<Eigen::Vector3d> traj_vis;
+      while (eval_t < time_sum) {
+        Eigen::Vector3d pt = poly_traj.evaluate(eval_t);
+        traj_vis.push_back(pt);
+        eval_t += 0.01;
+      }
+      displayPathWithColor(traj_vis, 0.1, Eigen::Vector4d(0, 1, 0, 1), 1);
+
+      // displayTrajectory(coeff, false);
+
       cout << "[2]: finish test." << endl;
     }
 
